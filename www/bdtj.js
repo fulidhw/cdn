f030fc75b83cd192edc77de5c987fbd7852c5b2a
wCloud <62798721+fulidhw@users.noreply.github.com>
2021-01-09 23:25:49 +0800

    Create bdtj.js

---
+++ www/bdtj.js
@@ -0,0 +1,9 @@
+document.writeln("<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement(\"script\");
+  hm.src = \"https:\/\/hm.baidu.com\/hm.js?3599eee415d8f8316d3667a04b425ce3\";
+  var s = document.getElementsByTagName(\"script\")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+<\/script>");
