6142bbbd9ccf901ad4e2346aa29c703f60d0f40a
wCloud <62798721+fulidhw@users.noreply.github.com>
2021-04-20 02:50:06 +0800

    Update tj.js

--- www/tj.js
+++ www/tj.js
@@ -1,3 +1,8 @@
-document.write ('<script type="text/javascript"  src="https://js.users.51.la/20676637.js"></script>');
-document.write ('<script type="text/javascript"  src="https://js.users.51.la/20522817.js"></script>');
-document.write ('<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1279231050'%3E%3C/span%3E%3Cscript src='https://v1.cnzz.com/z_stat.php%3Fid%3D1279231050' type='text/javascript'%3E%3C/script%3E"));</script>');
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?83e8d31cdb5e69ec1ea49ae7d6637bd8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+ 
