e71604c370d9f69a624fef609a93f4c402e11e14
wCloud <62798721+fulidhw@users.noreply.github.com>
2021-04-20 02:53:27 +0800

    Create uutj.js

---
+++ www/uutj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?83e8d31cdb5e69ec1ea49ae7d6637bd8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
